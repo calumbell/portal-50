06c94ee209a1aa7fd4af2ceb50cc1fe3fe8473ea
Calum Bell <calumabell@googlemail.com>
2020-10-07 13:13:11 +0100

    created respawn script

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Vector3Value spawnPoint;
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            transform.position = spawnPoint.value;
+        }
+    }
+}
