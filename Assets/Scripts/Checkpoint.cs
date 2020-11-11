01dac322396662d47c63442a66d1402203dbd67f
Calum Bell <calumabell@googlemail.com>
2020-11-11 13:22:52 +0000

    added checkpoint system

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Vector3Value playerSpawnLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerSpawnLocation.value = new Vector3(
+                gameObject.transform.position.x,
+                gameObject.transform.position.y + 4,
+                gameObject.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateGeometry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGeometry : MonoBehaviour
+{
+    public Vector3 m_EulerAngleVelocity;
+
+
+    void Update()
+    {
+
+    }
+}
