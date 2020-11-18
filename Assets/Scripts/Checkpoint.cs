d0641dab2d37feb31682baf9323b6364e0e0ca96
Calum Bell <calumabell@googlemail.com>
2020-11-18 11:52:22 +0000

    add UI prompt for triggering a checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,15 +3,32 @@
 public class Checkpoint : MonoBehaviour
 {
     public Vector3Value playerSpawnLocation;
+    private Vector3 checkpointLocation;
+
+    public delegate void CheckpointAction();
+    public static event CheckpointAction OnCheckpointActivated;
+
+    private void Start()
+    {
+        // Apply offset to respawn location
+        checkpointLocation = new Vector3(
+            gameObject.transform.position.x + 0,
+            gameObject.transform.position.y + 4,
+            gameObject.transform.position.z + 0);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            playerSpawnLocation.value = new Vector3(
-                gameObject.transform.position.x,
-                gameObject.transform.position.y + 4,
-                gameObject.transform.position.z);
-        }
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+
+        if (playerSpawnLocation.value == checkpointLocation)
+            return;
+
+        playerSpawnLocation.value = checkpointLocation;
+
+        if (OnCheckpointActivated != null)
+            OnCheckpointActivated();
     }
 }
+
---
+++ Assets/Scripts/UITitleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITitleScript : MonoBehaviour
+{
+    public Text title;
+
+    private void OnEnable()
+    {
+        Checkpoint.OnCheckpointActivated += CheckpointUpdateMessage;
+    }
+
+    private void OnDisable()
+    {
+        Checkpoint.OnCheckpointActivated -= CheckpointUpdateMessage;
+    }
+
+    void CheckpointUpdateMessage()
+    {
+        if (title == null)
+            return;
+
+        StartCoroutine(CheckpointUpdateMessageCoroutine());
+    }
+
+    private IEnumerator CheckpointUpdateMessageCoroutine()
+    {
+        title.text = "Checkpoint Triggered!";
+        title.enabled = true;
+
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            title.enabled = !title.enabled;
+        }
+
+        title.enabled = false;
+        
+    }
+}
